fb8ee98c4e1f39802e5a360a7dcac0180ec99522
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-01-10 14:57:01 +0100

    adds initial complete backprojection

--- 4_algebraic_reconstruction_technique/art.m
+++ 4_algebraic_reconstruction_technique/art.m
@@ -97,10 +97,12 @@ fprintf("Normalization Value: %.2f\n", h4)
 fprintf("\n")
 
 %% Part 3 - Conversion to the x/y-system
+
 % Extended the normalization factor from line_integral_rc.m to
 % line_integral_xy.m
 
 %% Part 4 - Forward Projection for One View
+
 % Created view_xy.m from view.m to include normalization factors
 
 %% Part 5 - Backprojection
@@ -166,163 +168,65 @@ fprintf("\n")
 
 %% Part 6 - Backprojection for One View
 
+% Created backproject_view_xy.m from view_xy.m, implemented backprojection
+% and called backproject_xy
+
 %% Part 7 - The Complete Reconstruction
 
-%% Old
-% %% Part 1 - Basic coordinate system and its orientation
+% Load image
+ct_data = rand(200,200);
+
+% Initialize Image Reconstruction
+image = zeros(1);
+
+% Store image as the older version of itself
+old_image = image;
+
+% % CT machine geometry
+% FCD_mm = 400;
+% DCD_mm = 200;
+% detector_width_mm = 400;
+% image_width = 200;
 % 
-% %% Part 2 - Adding x/y system
+% % Iterate backprojections to reconstruct image 
+% for iter=1:n_iter
+%     for view=1:100 % select a random view, change 1:100
+%         % Calculate  and normalization values
+%         [s,h] = view_xy(old_image, FCD_mm);
 % 
-% data = zeros(5,5);
-% x_to_c( 6.0, data, 3.0); % result: 5
-% x_to_c( 0.0, data, 3.0); % result: 3
-% x_to_c(-7.5, data, 3.0); % result: 0.5
-% y_to_r( 0.0, data, 6.0); % result: 3
-% y_to_r(15.0, data, 6.0); % result: 0.5
+%         % Calculate measures projection values for one view
+%         m = sinogram(view);
 % 
-% %% Part 3 - Create x-ray source
+%         % Calculate the difference between simulated and measured 
+%         % projection values
+%         d = s - m;
 % 
-% [new_x,new_y] = rotate_xy(0,1,270);
+%         % Backproject new image
+%         backproject = backproject_view_xy(image_data, FCD_mm, DCD_mm, ...
+%             angle_deg, n_dexel, dexel_size_mm, pixel_size_mm, d, h);
 % 
-% FCD_mm = 10;
-% [x,y] = tube_position_xy(FCD_mm, 0);
-% disp(x), disp(y)
-% [x,y] = tube_position_xy(FCD_mm, 135);
-% disp(x), disp(y)
-% [x,y] = tube_position_xy(FCD_mm, -45);
-% disp(x), disp(y)
-% 
-% %% Part 4 - Create the detector
-% 
-% DCD_mm = 100;
-% angle_deg = 45;
-% n_dexel = 10;
-% dexel_size_mm = 10;
-% 
-% [x,y] = detector_position_xy(DCD_mm, angle_deg, n_dexel, dexel_size_mm);
-% plot(x,y,'r*')
-% xlim([-150, 150])
-% ylim([-150, 150])
-% title('Detector Array')
-% 
-% %% Part 5 - Line integral through an array
-% 
-% data = [0, 0, 0, 0
-%         0, 5, 2, 0
-%         0, 1, 3, 0
-%         0, 0, 0, 0];
-% 
-% source_r=1.7; source_c=4.5; dexel_r=1.7; dexel_c=0.5; % p -> 7.15
-% % source_r=3.0; source_c=0.5; dexel_r=3.0; dexel_c=4.5; % p -> 4.15
-% % source_r=4.5; source_c=0.5; dexel_r=0.5; dexel_c=4.5; % p -> 4.20
-% % source_r=4.5; source_c=3.5; dexel_r=0.5; dexel_c=1.5; % p -> 9.05
-% 
-% p = line_integral_rc(data, source_r, source_c, dexel_r, dexel_c);
-% disp(p)
-% 
-% %% Part 6 - Extend the integration function
-% 
-% data = [0, 0, 0, 0
-%         0, 5, 2, 0
-%         0, 1, 3, 0
-%         0, 0, 0, 0];
-% 
-% source_x=7.5; source_y=1.7; dexel_x=-7.5; dexel_y=1.7; pixel_size_mm = 3; 
-%     % p -> 7.15
-% 
-% % p2 = line_integral_xy(data, pixel_size_mm,source_x, source_y, dexel_x, dexel_y);
-% 
-% % convert the source coordinates from x,y to r,c
-% source_c = x_to_c(source_x, data, pixel_size_mm);
-% source_r = y_to_r(source_y, data, pixel_size_mm);
-% 
-% % convert the detector coordinates from x,y to r,c
-% dexel_c = x_to_c(dexel_x, data, pixel_size_mm);
-% dexel_r = y_to_r(dexel_y, data, pixel_size_mm);
-% 
-% % perform line integration
-% p2 = line_integral_rc(data,source_r, source_c, dexel_r, dexel_c);
-% 
-% %% Part 7 - Simulation for one view
-% 
-% image = imread('CTLab-Introduction3.jpg');
-% 
-% image = image(:,:,1);
-% 
-% image = double(image);
-% image = image / max(max(image));
-% image = image * 0.4;
-% 
-% [n_r, ~] = size(image);
-% n_pixel = n_r;
-% 
-% FCD_mm = 300;
-% DCD_mm = 300;
-% 
-% angle_deg=45;
-% 
-% n_dexel=200;
-% 
-% image_size_mm = 300;
-% 
-% detector_size_mm = 600;
-% 
-% dexel_size_mm = detector_size_mm / n_dexel;
-% pixel_size_mm = image_size_mm / n_pixel;
-% 
-% p = view(image, FCD_mm, DCD_mm, angle_deg, n_dexel, dexel_size_mm, pixel_size_mm);
-% 
-% figure()
-% plot(p)
-% title('Projection')
-% 
-% figure()
-% imagesc(image)
-% title('Initial Image')
-% colormap gray(256)
-% axis off
-% 
-% %% Part 8 - Complete Simulation
-% 
-% image = imread('CTLab-Introduction3.jpg');
-% 
-% image = image(:,:,1);
-% 
-% image = double(image);
-% image = image / max(max(image));
-% image = image * 0.4;
-% 
-% [n_r, n_x] = size(image);
-% n_pixel = n_r;
-% 
-% FCD_mm = 300;
-% DCD_mm = 300;
-% 
-% n_dexel=200;
-% 
-% image_size_mm = 300;
-% 
-% detector_size_mm = 600;
-% 
-% dexel_size_mm = detector_size_mm / n_dexel;
-% pixel_size_mm = image_size_mm / n_pixel;
-% 
-% angles_deg = 0:2:359;
-% 
-% sino = simulation(image, FCD_mm, DCD_mm, angles_deg, n_dexel, dexel_size_mm, pixel_size_mm);
-% 
-% figure()
-% subplot(1,2,1)
-% imagesc(image)
-% colormap gray(256)
-% title('Image')
-% axis('square')
-% axis off
-% 
-% subplot(1,2,2)
-% sinogram_array = projection(image);
-% imagesc(sino)
-% colormap gray(256) 
-% title('Sinogram')
-% axis('square')
-% axis off
+%         % Find new image with back projection
+%         image = image - backproject;
+%     end
+%     % Replace old image with current reconstruction
+%     old_image = image;
+% end
+
+% Create Dual Plot of Original Image and Backprojection
+figure()
+% Plot original CT data
+subplot(1,2,1)
+imagesc(ct_data)
+colormap gray(256)
+title('CT Data')
+axis('square')
+axis off
+
+% Plot Reconstruction
+subplot(1,2,2)
+sinogram_array = projection(image);
+imagesc(image)
+colormap gray(256) 
+title('Reconstruction')
+axis('square')
+axis off
