0ecc1710e6fab974fc905dfe2a1b423f484be674
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-01-15 02:36:52 +0100

    updates line_integral_rc

--- 4_algebraic_reconstruction_technique/line_integral_rc.m
+++ 4_algebraic_reconstruction_technique/line_integral_rc.m
@@ -10,80 +10,47 @@ function [s, h] = line_integral_rc(data, source_r, source_c, dexel_r, dexel_c)
 % s - attenuation signal for the x-ray beam at the detector
 % h - normalization factor
 
-% Extract size of data 
-[pixel_r, pixel_c] = size(data);
+% extract size of data for limits to simulation
+[data_x,data_y] = size(data);
 
-% Source Location
-beam_start = [source_c source_r];
-
-% End Location
-beam_end = [dexel_c dexel_r];
-
-% Beam Vector
-n_datapoints = 10000;
-beam_r = [linspace(beam_start(1),beam_end(1), n_datapoints)];
-beam_c = [linspace(beam_start(2),beam_end(2), n_datapoints)];
-
-% Initialize attenuation variables
+% Initiate attenuation variables to iterate a summation
 s = 0; % total attenuation
 a = zeros(1); % row vector of pixel intersection lengths
 
-counter = 0;
+% source location
+b = [source_c source_r];
 
-% Iterate through all pixels within the data array
-for r=1:pixel_r
-    for c=1:pixel_c
-        i_start = false;
-        % For all points along the Beam
-        for i=1:size(beam_r,2)
-            % If the r-values and c-values are within pixel range
-            if beam_r(i)<r+0.5 && beam_r(i)>=r-0.5 && beam_c(i)<c+0.5 && beam_c(i)>=c-0.5
-                % Save the intersection points
-                % (first and last points within pixel range)
-                % If this is the first time an intersection has occured
-                if i_start == false
-                    % Save the r.c coords for first point within range
-                    inter_points(1,1) = beam_c(i);
-                    inter_points(1,2) = beam_r(i);
-                    inter_points(2,1) = beam_c(i);
-                    inter_points(2,2) = beam_r(i);
-                    i_start = true;
-                % If intersection has already been detected
-                elseif i_start == true
-                    % Store the latest point within range
-                    inter_points(2,1) = beam_c(i);
-                    inter_points(2,2) = beam_r(i);
-                end
+% x-ray beam vector
+d = [dexel_c-source_c dexel_r-source_r];
 
-            % If intersection started and the point is not within pixel
-            elseif i_start == true
-                % Calculate Correction for resolution gap
-                corr = 3.9/n_datapoints;
+% x-ray beam vector magnitude
+norm_scalar = sqrt( (dexel_c-source_c)^2 + (dexel_r-source_r)^2 );
 
-                % disp(inter_points)
+% normalized x-ray beam vector
+d_norm = d/norm_scalar;
 
-                % Calculate r-length of beam within pixel
-                r_length = abs(inter_points(1,2)-inter_points(2,2)) + corr;
-                % Calculate c-length of beam within pixel
-                c_length = abs(inter_points(1,1)-inter_points(2,1)) + corr;
-                % Calculate total leangth of beam within pixel
-                s_pixel = sqrt(r_length.^2 + c_length.^2);
+% iteration 
+delta_s = .05;
 
-                % Find Attenuation value and add it to the running total
-                s = s + s_pixel*data(c,r);
-                
-                
-                counter = counter + 1;
-                a(counter) = s_pixel;
+% for loop to iterate value of s over entire d vector
+for i = 0:norm_scalar/delta_s
+    c = round(b(1)); % c coordinate for the data vector at current position
+    r = round(b(2)); % r coordinate for the data vector at current position
+    b = b + delta_s*d_norm; % increase point b from source to detector
 
-                % Exit from calculation of current pixel
-                break
-            end
-        
-        end
+    % increase p only if position is within the data matrix
+    if (0<c) && (c<=data_x) && (0<r) && (r<=data_y)
+        length = length + delta_s;
+        s = s + delta_s*data(r,c); % iterate the value of s
+        % a(counter) = s_pixel;
+        disp(s)
+    else
+        continue
     end
 end
 
+s = length
 h = a*(a.');
+h = 0;
 
 end
