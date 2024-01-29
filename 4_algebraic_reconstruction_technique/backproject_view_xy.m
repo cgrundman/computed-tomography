a2a0626d3324ea6065dad1e3f15fb047a889772b
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-01-29 16:49:11 +0100

    Update backproject_view_xy.m

--- 4_algebraic_reconstruction_technique/backproject_view_xy.m
+++ 4_algebraic_reconstruction_technique/backproject_view_xy.m
@@ -1,5 +1,5 @@
-function correction_image = backproject_view_xy(image_data, FCD_mm, ...
-    DCD_mm, angle_deg, n_dexel, dexel_size_mm, pixel_size_mm, d, h)
+function correction_image = backproject_view_xy(image, FCD_mm, DCD_mm, ...
+    angle_deg, n_dexel, dexel_size_mm, pixel_size_mm, d, h)
 % INPUTS
 % image_data - image matrix
 % FCD_mm - distance from x-ray source to center
@@ -12,10 +12,6 @@ function correction_image = backproject_view_xy(image_data, FCD_mm, ...
 % OUTPUTS
 % correction_image - backprojected image from above
 
-
-% Store image data
-image = image_data;
-
 % Calculate position of x-ray source
 [source_x,source_y] = tube_position_xy(FCD_mm, angle_deg);
 
@@ -24,20 +20,19 @@ image = image_data;
                                      n_dexel, dexel_size_mm);
 
 % Initialize correction_image
-correction_image = zeros(size(image));
-
-% disp("backproject_view_xy h")
-% disp(h)
+correction_image = zeros(n_dexel,size(image,1),size(image,2)); % Create vector having the size of the Dexel amount
 
 % Iterate over all detectors to calculate attenuation array
 for i = 1:n_dexel
-    % store each single measuerment in P
-    correction_image(:,:) = correction_image + backproject_xy(image, source_x, source_y, ...
-    dexel_x(n_dexel), dexel_y(n_dexel), pixel_size_mm, d(i), h(i));
+    backproject = backproject_xy(image,source_x, source_y, dexel_x(1), dexel_y(1),pixel_size_mm, d(1), h); % Create correcton data for one dexel
     
+    % Reshape Correction Image
+    backproject = reshape(backproject, size(image));
+
+    % Store current iteration
+    correction_image(i,:,:) = backproject; % Fill vector at dexel position idx
 end
 
-% disp("backproject_view_xy")
-% disp(correction_image)
+correction_image = squeeze(sum(correction_image, 1));
 
 end
