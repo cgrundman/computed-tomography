63931fc6961722201cb6c099931f32d60978fbb7
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-01-30 20:16:02 +0100

    update metal_artifact_removal.m with reconstruction

--- 5_metal_artifact_reduction/metal_artifact_removal.m
+++ 5_metal_artifact_reduction/metal_artifact_removal.m
@@ -8,19 +8,52 @@ close all
 %% Load Data
 % Load Sinogram
 data = load('hip_sino.mat');
-hip_sino = data.sino;
+data_sino = data.sino;
 
 % Visualize Sinogram
 figure()
-imagesc(hip_sino)
+imagesc(data_sino)
 colormap gray(256)
-img_title = "Hip Sinogram with Metal Prosthesis";
+img_title = "Hip with Metal Prosthesis - Sinogram";
 title(img_title,'FontSize',16)
 axis off
 
-%% Creation of Metal Mask
-% Run the metal_mask.m file
-metal_mask;
+%% Reconstruct the Data
+% Perform reconstruction
+data_reco = reconstruct(data_sino);
+
+% Set thresholds for visualization
+level = 0.014;
+window = 0.007;
+vmin = level - window/2;
+vmax = level + window/2;
+
+% Visualize Reconstruction
+figure();
+imagesc(data_reco, [vmin vmax]);
+colormap gray(256)
+img_title = "Hip with Metal Prosthesis - CT Image";
+title(img_title,'FontSize',16)
+axis('square')
+% axis off
+xticklabels ''
+yticklabels ''
+
+%% Create Metal Mask
+
+img = imadjust(data_reco);  % contrasting
+bw = edge(data_reco,"canny",0.02,2); % edge detection with Canny method
+bw = imfill(bw,'holes'); % fill closed areas on the image
+%  figure();
+%  imshow(bw);
+se = strel('disk',1); % form a structural element
+bw = imopen(bw,se);  % morphological opening, lines disappear 
+figure();
+imshow(bw);
+fp_mask = forwardproject(bw); % forwardproject the mask
+% this is our mask
+figure();
+imshow(fp_mask);
 
 % Set threshholds for mask
 level = 0.014;
