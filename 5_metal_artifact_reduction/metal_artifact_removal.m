7bc8c02d008c802b0bb274fc4ece2d63d2c4484c
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-01-30 21:16:59 +0100

    updates visualizations and metal mask creation

--- 5_metal_artifact_reduction/metal_artifact_removal.m
+++ 5_metal_artifact_reduction/metal_artifact_removal.m
@@ -11,7 +11,7 @@ data = load('hip_sino.mat');
 data_sino = data.sino;
 
 % Visualize Sinogram
-figure()
+figure('units','normalized','outerposition',[0 0 .6 .75])
 imagesc(data_sino)
 colormap gray(256)
 img_title = "Hip with Metal Prosthesis - Sinogram";
@@ -29,7 +29,7 @@ vmin = level - window/2;
 vmax = level + window/2;
 
 % Visualize Reconstruction
-figure();
+figure('units','normalized','outerposition',[0 0 .4 .75]);
 imagesc(data_reco, [vmin vmax]);
 colormap gray(256)
 img_title = "Hip with Metal Prosthesis - CT Image";
@@ -40,34 +40,59 @@ xticklabels ''
 yticklabels ''
 
 %% Create Metal Mask
+% Adjust image for les saturation
+img = imadjust(data_reco);
 
-img = imadjust(data_reco);  % contrasting
-bw = edge(data_reco,"canny",0.02,2); % edge detection with Canny method
-bw = imfill(bw,'holes'); % fill closed areas on the image
-%  figure();
-%  imshow(bw);
-se = strel('disk',1); % form a structural element
-bw = imopen(bw,se);  % morphological opening, lines disappear 
-figure();
-imshow(bw);
-fp_mask = forwardproject(bw); % forwardproject the mask
-% this is our mask
-figure();
-imshow(fp_mask);
+% Perform Canny Edge Detection (shows borders of metal)
+canny = edge(data_reco, "canny", 0.02, 2);
 
-% Set threshholds for mask
-level = 0.014;
-window = 0.007;
 
-% Max and min thresholds for Mask
-vmin = level - window/2;
-vmax = level + window/2;
+% Fill closed regions within image (areas that are metal)
+canny_regions = imfill(canny,'holes'); % fill closed areas on the image
 
-% Visulaize Mask
-figure();
-imagesc(fp_mask, [vmin vmax]);
+
+% Remove remaining edges from Canny Edge Detection (false readings)
+kernel = strel('disk',1);
+canny_clean = imopen(canny_regions, kernel);
+
+
+% Create Sinogram of mask with forward projection
+mask_sino = forwardproject(canny_clean);
+
+% Visualize Metal Artifact Detection
+figure('units','normalized','outerposition',[0 0 1 .66]);
+subplot(1, 3, 1);
+imagesc(canny);
 colormap gray(256)
-img_title = "Metal Mask";
+img_title = "Canny Edge Detection";
+title(img_title,'FontSize',16)
+axis('square')
+xticklabels ''
+yticklabels ''
+
+subplot(1, 3, 2);
+imagesc(canny_regions);
+colormap gray(256)
+img_title = "Filled Regions";
+title(img_title,'FontSize',16)
+axis('square')
+xticklabels ''
+yticklabels ''
+
+subplot(1, 3, 3);
+imagesc(canny_clean);
+colormap gray(256)
+img_title = "Isolated Metal Regions";
+title(img_title,'FontSize',16)
+axis('square')
+xticklabels ''
+yticklabels ''
+
+% Visualize Metal Mask as a Sinogram
+figure('units','normalized','outerposition',[0 0 .6 .75]);
+imagesc(mask_sino);
+colormap gray(256)
+img_title = "Isolated Metal Regions - Sinogram";
 title(img_title,'FontSize',16)
 axis off
 
