29bbf1db63658d70dc5a9c4747fc7e1b76495f35
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-01-30 19:43:47 +0100

    add metal mask

--- 5_metal_artifact_reduction/metal_artifact_removal.m
+++ 5_metal_artifact_reduction/metal_artifact_removal.m
@@ -6,6 +6,7 @@ clear
 close all
 
 %% Load Data
+% Load Sinogram
 data = load('hip_sino.mat');
 hip_sino = data.sino;
 
@@ -16,3 +17,24 @@ colormap gray(256)
 img_title = "Hip Sinogram with Metal Prosthesis";
 title(img_title,'FontSize',16)
 axis off
+
+%% Creation of Metal Mask
+% Run the metal_mask.m file
+metal_mask;
+
+% Set threshholds for mask
+level = 0.014;
+window = 0.007;
+
+% Max and min thresholds for Mask
+vmin = level - window/2;
+vmax = level + window/2;
+
+% Visulaize Mask
+figure();
+imagesc(fp_mask, [vmin vmax]);
+colormap gray(256)
+img_title = "Metal Mask";
+title(img_title,'FontSize',16)
+axis off
+
