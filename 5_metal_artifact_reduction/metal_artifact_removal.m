06f5c427a4b0037239c03298f87c15f210fea40d
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-02-03 18:18:55 +0100

    add hip_mask and phantom_mask to main file

--- 5_metal_artifact_reduction/metal_artifact_removal.m
+++ 5_metal_artifact_reduction/metal_artifact_removal.m
@@ -5,7 +5,14 @@ clc
 clear
 close all
 
+%% Metal Pask Creation
 
+% Run hip_mask.m
+hip_mask();
 
+% Run phantom_mask.m
+phantom_mask();
 
+%% Noise Reduction
 
+%% Missing Information Interpolation
