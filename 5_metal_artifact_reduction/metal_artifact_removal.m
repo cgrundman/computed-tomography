b0f267dd03f06aec4398fd3f044a21d2d1e7097b
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-02-03 18:30:39 +0100

    load and display original data

--- 5_metal_artifact_reduction/metal_artifact_removal.m
+++ 5_metal_artifact_reduction/metal_artifact_removal.m
@@ -7,8 +7,14 @@ close all
 
 %% Metal Pask Creation
 
+% Set given windows for data values
+level = 0.014;
+window = 0.01;
+vmin = level - window / 2;
+vmax = level + window / 2;
+
 % Run hip_mask.m
-hip_mask();
+hip_mask(vmin, vmax);
 
 % Run phantom_mask.m
 phantom_mask();
